cb19bb99cce1cb5ca1cb50b4ea012e217c0d1bd0
ArshitaKamthann <kamthanarshita@gmail.com>
2025-01-24 21:43:36 +0530

    Add files via upload

---
+++ sign.html/python/new.py
@@ -0,0 +1,10 @@
+      #HELLO WORLD
+# print("hello world!")
+
+      #VARIABLE AND INPUT
+# name = input("Enter your name:")
+# print("Hello " + name + "!" )
+
+
+      #ARITHMETIC OPERATIONS
+num1 = int(input("Enter first number:"))
